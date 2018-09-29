72d57eac1bbaef4740d3c80988fbd17700c9daee
karladjm <karladjmartinez@gmail.com>
2018-09-29 07:36:06 -0500

    mainpage

--- client/src/components/mainpage.js
+++ client/src/components/mainpage.js
@@ -4,7 +4,7 @@ const Main = props => (
             <h1 className="text-center">The New York Times</h1>
             <h2 className="text-center">
             </h2>
-            <h4 className="text-center">Search for articles of interest.</h4>
+            <h4 className="text-center">Search for an article</h4>
         </div>
     </div>
 
